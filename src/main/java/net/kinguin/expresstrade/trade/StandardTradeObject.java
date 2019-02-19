40e417d0664fa983537fc184184ab63d9af8eed8
Jędrzej Witaszak <jedrzej@witaszak.pl>
2019-02-19 23:43:01 +0100

    add missing constructors

--- src/main/java/net/kinguin/expresstrade/trade/StandardTradeObject.java
+++ src/main/java/net/kinguin/expresstrade/trade/StandardTradeObject.java
@@ -1,11 +1,16 @@
 package net.kinguin.expresstrade.trade;
 
 import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import net.kinguin.expresstrade.item.StandardItemObject;
 
 @Data
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
 @Builder
 public class StandardTradeObject {
 
@@ -23,6 +28,8 @@ public class StandardTradeObject {
   private Boolean sent_by_you;
 
   @Data
+  @NoArgsConstructor(access = AccessLevel.PUBLIC)
+  @AllArgsConstructor(access = AccessLevel.PUBLIC)
   @Builder
   public static class TradeUser {
 
