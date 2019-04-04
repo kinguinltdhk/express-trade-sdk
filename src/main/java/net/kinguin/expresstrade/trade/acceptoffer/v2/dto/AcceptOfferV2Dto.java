97047ff24f602b940c3fdad65c6a01f054c5814b
Jędrzej Witaszak <jedrzej@witaszak.pl>
2019-04-04 13:02:36 +0200

    fix acceptofferv2 dto

--- src/main/java/net/kinguin/expresstrade/trade/acceptoffer/v2/dto/AcceptOfferV2Dto.java
+++ src/main/java/net/kinguin/expresstrade/trade/acceptoffer/v2/dto/AcceptOfferV2Dto.java
@@ -5,20 +5,22 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import net.kinguin.expresstrade.http.dto.ResponseDto;
 import net.kinguin.expresstrade.item.StandardItemObject;
+import net.kinguin.expresstrade.item.StandardItemObjectV2;
 import net.kinguin.expresstrade.trade.StandardTradeObject;
+import net.kinguin.expresstrade.trade.StandardTradeObjectV2;
 
 @Data
 @EqualsAndHashCode(callSuper = false)
 public class AcceptOfferV2Dto extends ResponseDto {
 
-  private AcceptOfferResponseDto response;
+  private AcceptOfferV2ResponseDto response;
 
   @Data
-  public static class AcceptOfferResponseDto {
+  public static class AcceptOfferV2ResponseDto {
 
-    private StandardTradeObject offer;
+    private StandardTradeObjectV2 offer;
 
-    private List<StandardItemObject> new_items;
+    private List<StandardItemObjectV2> new_items;
 
     private Integer failed_cases;
   }
